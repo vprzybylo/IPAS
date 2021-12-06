26847c1e4162f54061236b979a5b28a7d1bbb489
vprzybylo <vprzybylo@albany.edu>
2021-12-06 11:58:10 -0500

    ridgeplots for each var

--- ipas/CPI/campaign_data.py
+++ ipas/CPI/campaign_data.py
@@ -39,6 +39,7 @@ class CPI:
         self.a = []  # larger radius
         self.c = []  # smaller radius
         self.Dmax = []  # max diameter
+        self.aspect_ratios = []
         self.complexity = []
         self.all_campaigns = [
             "AIRS_II",
@@ -117,6 +118,8 @@ class CPI:
             "Contour Area": self.concat(self.cnt_area),
             "a": self.concat(self.a),
             "c": self.concat(self.c),
+            "Aspect Ratio": np.array(self.concat(self.c))
+            / np.array(self.concat(self.a)),
             "Dmax": self.concat(self.Dmax),
             "Complexity": self.concat(self.complexity),
         }
@@ -135,103 +138,3 @@ class CPI:
             self.df_CPI.replace([np.inf, -np.inf], np.nan).notnull().all(axis=1)
         ]
         self.df_CPI.dropna(inplace=True)
-
-
-@auto_str
-class Plot:
-    def __init__(self, df_CPI):
-        self.df_CPI = df_CPI
-
-    def campaign_subplots(self, var="Aspect Ratio"):
-        colors_cpi = ["#890000", "#ae3d00", "#ba9600", "#3b5828", "#014c63"]
-        particle_types = [
-            "agg",
-            "bullet",
-            "column",
-            "planar_polycrystal",
-            "compact_irreg",
-        ]
-        fig, axs = plt.subplots(2, 7, figsize=(25, 10), sharex=True, sharey=True)
-        axs = axs.ravel()
-
-        for c, campaign in enumerate(self.df_CPI["Campaign"].unique()):
-            self.df_CPI["Aspect Ratio"] = self.df_CPI["c"] / self.df_CPI["a"]
-            df = self.df_CPI[self.df_CPI["Campaign"] == campaign]
-            for i, part_type in enumerate(particle_types):
-                df_type = df[var][df["Classification"] == part_type]
-                part_type = (
-                    "compact irregular" if part_type == "compact_irreg" else part_type
-                )
-                part_type = (
-                    "planar polycrystal"
-                    if part_type == "planar_polycrystal"
-                    else part_type
-                )
-
-                sns.kdeplot(
-                    df_type,
-                    color=colors_cpi[i],
-                    label=part_type,
-                    ax=axs[c],
-                    linewidth=3,
-                )
-                no_ylab = [1, 2, 3, 5, 6, 7]
-                axs[c].set_ylabel(" " if c in no_ylab else "Density")
-                axs[c].set_title(f"{campaign}\n n={len(df):,}")
-                axs[c].yaxis.set_major_locator(MaxNLocator(integer=True))
-                axs[c].set_xlim(0.0, 1.0)
-        y = -0.5
-        x = -3.3
-        axs[c].legend(
-            bbox_to_anchor=(x, y),
-            loc="lower center",
-            ncol=5,
-            title="Particle Classification",
-            fontsize=14,
-        )
-        plt.rcParams["legend.title_fontsize"] = "xx-large"
-        plt.savefig(f"../plots/campaign_{var}.png", bbox_inches="tight")
-
-    def boxplot(self, df, var, ax):
-        sns.boxplot(
-            x="binned",
-            y=var,
-            data=df,
-            fliersize=0,
-            showmeans=True,
-            meanprops={
-                "marker": "o",
-                "markerfacecolor": "white",
-                "markeredgecolor": "black",
-                "markersize": "10",
-            },
-            ax=ax,
-        )
-
-    def complexity_area_ratio(self, part_type="agg"):
-        """
-        complexity (assuming increasing number of monomers with increase in C)
-        vs. area ratio and aspect ratio in subplot
-        """
-        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(5, 10), sharex=True)
-
-        # truncate CPI dataframe for a specific particle type
-        df = self.df_CPI[self.df_CPI["Classification"] == part_type].copy()
-
-        # bin complexity into 10 linearly spaced bins
-        df["binned"] = pd.cut(df["Complexity"], bins=np.linspace(0.0, 1.0, 10))
-        # df["binned"] = pd.qcut(df['complexity'], 15)
-
-        # plot aspect ratio for each complexity bin
-        self.boxplot(df, var="Aspect Ratio", ax=ax1)
-        plt.xticks(rotation=90)
-        ax1.set_ylabel("Aspect Ratio")
-        ax1.set_xlabel("")
-
-        # plot area ratio for each complexity bin
-        self.boxplot(df, var="Area Ratio", ax=ax2)
-        plt.xticks(rotation=90)
-        plt.ylabel("Area Ratio")
-        plt.xlabel("Complexity")
-        plt.savefig("../plots/complexity_phi_ar.png", bbox_inches="tight")
-        # each complexity bin count varies between 138 and 80758
